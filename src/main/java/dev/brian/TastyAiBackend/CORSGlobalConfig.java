7070db0b259266c0a54864091992567630cabea6
bwbowen15 <bwbowen15@gmail.com>
2025-08-02 18:00:28 -0400

    trying to get it to work on deployment

---
+++ src/main/java/dev/brian/TastyAiBackend/CORSGlobalConfig.java
@@ -0,0 +1,25 @@
+package dev.brian.TastyAiBackend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CORSGlobalConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+
+    }
+}
